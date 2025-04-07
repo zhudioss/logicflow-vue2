822bcddaeec102fa13aa83d9e1e25e4e7d0f0321
diz <zhudioss@163.com>
2025-04-07 09:46:25 +0800

    提交dist

---
+++ dist/js/about.af71582d.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkproject_v2"]=self["webpackChunkproject_v2"]||[]).push([[594],{9220:function(t,n,u){u.r(n),u.d(n,{default:function(){return l}});var e=function(){var t=this;t._self._c;return t._m(0)},s=[function(){var t=this,n=t._self._c;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],r=u(1656),a={},c=(0,r.A)(a,e,s,!1,null,null,null),l=c.exports}}]);
+//# sourceMappingURL=about.af71582d.js.map
