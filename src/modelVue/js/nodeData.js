b86e9d85d4508912e73d605a1bbe1a1f0a70ce27
diz <zhudioss@163.com>
2025-09-10 11:51:09 +0800

    临时数据

--- src/modelVue/js/nodeData.js
+++ src/modelVue/js/nodeData.js
@@ -631,16 +631,260 @@ export const nodeData =
     {
         "nodes": [
             {
-                "id": "1",
+                "id": "9290a818-89c0-4c85-8b7e-4602b6d3b69c",
+                "type": "start-v",
+                "x": 292.5,
+                "y": 68.75,
+                "properties": {
+                    "width": 240,
+                    "height": 50,
+                    "startInput": ""
+                }
+            },
+            {
+                "id": "b6d87207-a854-4471-b19c-a5c24457e25e",
                 "type": "branch-v",
-                "x": 747.5,
-                "y": 178.75,
+                "x": 644.9999999999999,
+                "y": 68.75,
                 "properties": {
                     "width": 240,
                     "height": 100,
+                    "hideAnchor": true,
+                    "startInput": ""
+                }
+            },
+            {
+                "id": "fc5950c9-2efd-4103-b5f7-7a84948319b0",
+                "type": "llm-v",
+                "x": 1046.25,
+                "y": -41.666666666666664,
+                "properties": {
+                    "width": 240,
+                    "height": 50,
+                    "hideAnchor": true,
+                    "startInput": ""
+                }
+            },
+            {
+                "id": "37060639-b681-4af9-9917-d69f9498c1b9",
+                "type": "knowledge-v",
+                "x": 1045,
+                "y": 171.66666666666669,
+                "properties": {
+                    "width": 240,
+                    "height": 50,
+                    "hideAnchor": true,
+                    "startInput": ""
+                }
+            },
+            {
+                "id": "30b1a11a-aa2a-466c-a617-32e8f0449abb",
+                "type": "end-v",
+                "x": 1426.25,
+                "y": -42.916666666666664,
+                "properties": {
+                    "width": 240,
+                    "height": 50,
+                    "hideAnchor": true,
+                    "startInput": ""
+                }
+            },
+            {
+                "id": "f01ff1e1-62d0-41e7-a2d6-4d2221e4c851",
+                "type": "end-v",
+                "x": 1426.25,
+                "y": 170.41666666666669,
+                "properties": {
+                    "width": 240,
+                    "height": 50,
+                    "hideAnchor": true,
                     "startInput": ""
                 }
             }
         ],
-        "edges": []
+        "edges": [
+            {
+                "id": "b100cab4-be94-48b6-acc6-c2da416aafe7",
+                "type": "EDGE_BEZIER",
+                "properties": {
+                    "showAddMark": false
+                },
+                "sourceNodeId": "9290a818-89c0-4c85-8b7e-4602b6d3b69c",
+                "targetNodeId": "b6d87207-a854-4471-b19c-a5c24457e25e",
+                "sourceAnchorId": "right_9290a818-89c0-4c85-8b7e-4602b6d3b69c",
+                "targetAnchorId": "left_b6d87207-a854-4471-b19c-a5c24457e25e",
+                "startPoint": {
+                    "x": 412.5,
+                    "y": 68.75
+                },
+                "endPoint": {
+                    "x": 524.9999999999999,
+                    "y": 68.75
+                },
+                "pointsList": [
+                    {
+                        "x": 412.5,
+                        "y": 68.75
+                    },
+                    {
+                        "x": 512.5,
+                        "y": 68.75
+                    },
+                    {
+                        "x": 424.9999999999999,
+                        "y": 68.75
+                    },
+                    {
+                        "x": 524.9999999999999,
+                        "y": 68.75
+                    }
+                ]
+            },
+            {
+                "id": "01c99117-ed55-4696-b881-6d7e2ec798ab",
+                "type": "EDGE_BEZIER",
+                "properties": {
+                    "showAddMark": false
+                },
+                "sourceNodeId": "b6d87207-a854-4471-b19c-a5c24457e25e",
+                "targetNodeId": "fc5950c9-2efd-4103-b5f7-7a84948319b0",
+                "sourceAnchorId": "right_0_b6d87207-a854-4471-b19c-a5c24457e25e",
+                "targetAnchorId": "left_fc5950c9-2efd-4103-b5f7-7a84948319b0",
+                "startPoint": {
+                    "x": 764.9999999999999,
+                    "y": 52.083333333333336
+                },
+                "endPoint": {
+                    "x": 926.25,
+                    "y": -41.666666666666664
+                },
+                "pointsList": [
+                    {
+                        "x": 764.9999999999999,
+                        "y": 52.083333333333336
+                    },
+                    {
+                        "x": 864.9999999999999,
+                        "y": 52.083333333333336
+                    },
+                    {
+                        "x": 826.25,
+                        "y": -41.666666666666664
+                    },
+                    {
+                        "x": 926.25,
+                        "y": -41.666666666666664
+                    }
+                ]
+            },
+            {
+                "id": "0b2d5b43-02b7-45f9-97b6-1a5a957a1af8",
+                "type": "EDGE_BEZIER",
+                "properties": {
+                    "showAddMark": false
+                },
+                "sourceNodeId": "b6d87207-a854-4471-b19c-a5c24457e25e",
+                "targetNodeId": "37060639-b681-4af9-9917-d69f9498c1b9",
+                "sourceAnchorId": "right_1_b6d87207-a854-4471-b19c-a5c24457e25e",
+                "targetAnchorId": "left_37060639-b681-4af9-9917-d69f9498c1b9",
+                "startPoint": {
+                    "x": 764.9999999999999,
+                    "y": 85.41666666666667
+                },
+                "endPoint": {
+                    "x": 925,
+                    "y": 171.66666666666669
+                },
+                "pointsList": [
+                    {
+                        "x": 764.9999999999999,
+                        "y": 85.41666666666667
+                    },
+                    {
+                        "x": 864.9999999999999,
+                        "y": 85.41666666666667
+                    },
+                    {
+                        "x": 825,
+                        "y": 171.66666666666669
+                    },
+                    {
+                        "x": 925,
+                        "y": 171.66666666666669
+                    }
+                ]
+            },
+            {
+                "id": "4b75a9cb-1df7-46c5-ace6-bc46f24796b8",
+                "type": "EDGE_BEZIER",
+                "properties": {},
+                "sourceNodeId": "fc5950c9-2efd-4103-b5f7-7a84948319b0",
+                "targetNodeId": "30b1a11a-aa2a-466c-a617-32e8f0449abb",
+                "sourceAnchorId": "right_fc5950c9-2efd-4103-b5f7-7a84948319b0",
+                "targetAnchorId": "left_30b1a11a-aa2a-466c-a617-32e8f0449abb",
+                "startPoint": {
+                    "x": 1166.25,
+                    "y": -41.666666666666664
+                },
+                "endPoint": {
+                    "x": 1306.25,
+                    "y": -42.916666666666664
+                },
+                "pointsList": [
+                    {
+                        "x": 1166.25,
+                        "y": -41.666666666666664
+                    },
+                    {
+                        "x": 1266.25,
+                        "y": -41.666666666666664
+                    },
+                    {
+                        "x": 1206.25,
+                        "y": -42.916666666666664
+                    },
+                    {
+                        "x": 1306.25,
+                        "y": -42.916666666666664
+                    }
+                ]
+            },
+            {
+                "id": "56213410-b954-4ae9-b9b2-1aef49f0a5b6",
+                "type": "EDGE_BEZIER",
+                "properties": {
+                    "showAddMark": false
+                },
+                "sourceNodeId": "37060639-b681-4af9-9917-d69f9498c1b9",
+                "targetNodeId": "f01ff1e1-62d0-41e7-a2d6-4d2221e4c851",
+                "sourceAnchorId": "right_37060639-b681-4af9-9917-d69f9498c1b9",
+                "targetAnchorId": "left_f01ff1e1-62d0-41e7-a2d6-4d2221e4c851",
+                "startPoint": {
+                    "x": 1165,
+                    "y": 171.66666666666669
+                },
+                "endPoint": {
+                    "x": 1306.25,
+                    "y": 170.41666666666669
+                },
+                "pointsList": [
+                    {
+                        "x": 1165,
+                        "y": 171.66666666666669
+                    },
+                    {
+                        "x": 1265,
+                        "y": 171.66666666666669
+                    },
+                    {
+                        "x": 1206.25,
+                        "y": 170.41666666666669
+                    },
+                    {
+                        "x": 1306.25,
+                        "y": 170.41666666666669
+                    }
+                ]
+            }
+        ]
     }
